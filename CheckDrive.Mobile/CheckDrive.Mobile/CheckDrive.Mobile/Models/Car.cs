85c59a6c9f63320b7685fbd20096f02215fe065d
Zikirillo <128654271+Zikirillo2002@users.noreply.github.com>
2024-05-08 18:55:18 +0500

    Added Car class

---
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace CheckDrive.Mobile.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string Number { get; set; }
+        public double MeduimFuelConsumption { get; set; }
+        public double FuelTankCapacity { get; set; }
+        public int ManufacturedYear { get; set; }
+
+        public virtual ICollection<MechanicHandover> MechanicHandovers { get; set; }
+    }
+}
