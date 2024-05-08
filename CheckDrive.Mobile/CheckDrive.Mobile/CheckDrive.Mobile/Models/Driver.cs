402ffd8ad5ce903eb89de3212d2bf3db47ca183b
Zikirillo <128654271+Zikirillo2002@users.noreply.github.com>
2024-05-08 18:56:48 +0500

    Added Driver class

---
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Models/Driver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace CheckDrive.Mobile.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+
+        public int AccountId { get; set; }
+        public Account Account { get; set; }
+
+        public virtual ICollection<DispatcherReview> DispetcherReviews { get; set; }
+        public virtual ICollection<DoctorReview> DoctorReviews { get; set; }
+        public virtual ICollection<MechanicHandover> MechanicHandovers { get; set; }
+        public virtual ICollection<OperatorReview> OperatorReviews { get; set; }
+    }
+}
