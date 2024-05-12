de3e44560b22d3d25619ce661a1d66a3499e2268
Sharafiddin.Mukhtorov <129146459+SharafiddinMukhtorov@users.noreply.github.com>
2024-05-12 18:09:18 +0500

    Created response

---
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Responses/ApiResponse.cs
@@ -0,0 +1,25 @@
+﻿using CheckDrive.Mobile.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace CheckDrive.Mobile.Responses
+{
+    public class ApiResponse<T>
+    {
+        public IEnumerable<T> Data { get; private set; }
+        public MetaData Metadata { get; private set; }
+
+        public ApiResponse()
+        {
+            Data = new List<T>();
+            Metadata = new MetaData();
+        }
+
+        public static implicit operator ApiResponse<T>(HttpResponseMessage v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
