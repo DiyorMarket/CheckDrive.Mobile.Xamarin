822383ab763578d7190b5d76a13d7c6b301aff9d
Zikirillo <128654271+Zikirillo2002@users.noreply.github.com>
2024-06-15 14:28:24 +0500

    Added refreshView for RoadMapPage

--- CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Views/RoadMapPage.xaml.cs
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Views/RoadMapPage.xaml.cs
@@ -4,9 +4,6 @@ using CheckDrive.Mobile.Stores.MechanicAcceptances;
 using CheckDrive.Mobile.Stores.MechanicHandovers;
 using CheckDrive.Mobile.Stores.OperatorReviews;
 using CheckDrive.Mobile.ViewModels;
-using CheckDrive.Web.Stores.DoctorReviews;
-using CheckDrive.Web.Stores.MechanicHandovers;
-using CheckDrive.Web.Stores.OperatorReviews;
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -16,8 +13,7 @@ namespace CheckDrive.Mobile.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RoadMapPage : ContentPage
     {
-        private RoadMapViewModel roadMapViewModel;
-
+        private RoadMapViewModel viewModel;
         public RoadMapPage()
         {
             InitializeComponent();
@@ -28,8 +24,15 @@ namespace CheckDrive.Mobile.Views
             var operatorReviewDS = new OperatorReviewDataStore(client);
             var mechanicAcceptanceDS = new MechanicAcceptanceDataStore(client);
 
-            BindingContext = roadMapViewModel = new RoadMapViewModel(doctorReviewDS, mechanicAcceptanceDS, operatorReviewDS, mechanicHandoverDS);
+            BindingContext = viewModel = new RoadMapViewModel(doctorReviewDS, mechanicAcceptanceDS, operatorReviewDS, mechanicHandoverDS);
         }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            viewModel.LoadViewPage();
+        }
+
         private async void ClickToShowPopup_Clicked(object sender, EventArgs e)
         {
             var popup = new CheckControlPopup()
@@ -38,6 +41,19 @@ namespace CheckDrive.Mobile.Views
             };
 
             await Navigation.PushModalAsync(popup);
+        }
+
+        private void RoadMapView_Refreshing(Object sender, EventArgs e)
+        {
+            var client = new ApiClient();
+            var doctorReviewDS = new DoctorReviewDataStore(client);
+            var mechanicHandoverDS = new MechanicHandoverDataStore(client);
+            var operatorReviewDS = new OperatorReviewDataStore(client);
+            var mechanicAcceptanceDS = new MechanicAcceptanceDataStore(client);
+
+            BindingContext = viewModel = new RoadMapViewModel(doctorReviewDS, mechanicAcceptanceDS, operatorReviewDS, mechanicHandoverDS);
+
+            RoadMapRefresh.IsRefreshing = false;
         }
     }
 }
