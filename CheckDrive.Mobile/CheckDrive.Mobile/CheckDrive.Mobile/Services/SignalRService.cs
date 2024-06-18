1cb6eb9e46bf6dd070b79af3c97d51147e9416a8
Zikirillo <128654271+Zikirillo2002@users.noreply.github.com>
2024-06-18 23:48:12 +0500

    Added SignalRservice class

---
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Services/SignalRService.cs
@@ -0,0 +1,92 @@
+﻿using CheckDrive.Mobile.Views;
+using Microsoft.AspNetCore.SignalR.Client;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace CheckDrive.Mobile.Services
+{
+    public class SignalRService
+    {
+        public ICommand SendResponseCommand { get; set; }
+
+        private HubConnection _hubConnection;
+
+        public SignalRService()
+        {
+            _hubConnection = new HubConnectionBuilder()
+                .WithUrl("https://2bvq12nl-7111.euw.devtunnels.ms/api/chat", options =>
+                {
+                    options.AccessTokenProvider = async () => await GetTokenAsync();
+                })
+            .Build();
+
+            _hubConnection.On<string>("ReceiveMessage", async (message) =>
+            {
+                await ShowPopupAsync(message);
+            });
+        }
+
+        public async Task SendResponse(bool isAccepted)
+        {
+            try
+            {
+                _hubConnection.SendAsync("ReceivePrivateResponse", isAccepted);
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public async Task StartConnectionAsync()
+        {
+            try
+            {
+                await _hubConnection.StartAsync();
+                Console.WriteLine("Соединение установлено.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        public async Task StopConnectionAsync()
+        {
+            try
+            {
+                await _hubConnection.StopAsync();
+                Console.WriteLine("Соединение закрыто.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        private async Task ShowPopupAsync(string message)
+        {
+                var popup = new CheckControlPopup(message);
+                await PopupNavigation.Instance.PushAsync(popup);
+        }
+
+
+        private async Task<string> GetTokenAsync()
+        {
+            try
+            {
+                return await SecureStorage.GetAsync("tasty-cookies");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при получении токена: {ex.Message}");
+                return string.Empty;
+            }
+        }
+
+    }
+}
