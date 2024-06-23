b788ed92eb6ebac91d272fb873852bbe46b1b2e6
Sharafiddin.Mukhtorov <129146459+SharafiddinMukhtorov@users.noreply.github.com>
2024-06-23 12:18:51 +0500

    Updated PersonalAccountPage

--- CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/ViewModels/PersonalAccountViewModel.cs
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/ViewModels/PersonalAccountViewModel.cs
@@ -31,20 +31,6 @@ namespace CheckDrive.Mobile.ViewModels
             get => _login;
             set => SetProperty(ref _login, value);
         }
-        private string _phone;
-        public string PhoneNumber
-        {
-
-            get => _phone;
-            set => SetProperty(ref _phone, value);
-        }
-        private string _login;
-        public string Login
-        {
-
-            get => _login;
-            set => SetProperty(ref _login, value);
-        }
 
         public PersonalAccountViewModel( )
         {
