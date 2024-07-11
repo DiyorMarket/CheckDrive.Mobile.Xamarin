00184b89f215fb6494794cae260dcc1d06898e82
Zikirillo <128654271+Zikirillo2002@users.noreply.github.com>
2024-07-11 16:55:25 +0500

    Create GetDispatcherReviewResponse class

---
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Responses/GetDispatcherReviewResponse.cs
@@ -0,0 +1,11 @@
+﻿using CheckDrive.ApiContracts.DispatcherReview;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckDrive.Mobile.Responses
+{
+    public class GetDispatcherReviewResponse : GetBaseResponse<DispatcherReviewDto>
+    {
+    }
+}
